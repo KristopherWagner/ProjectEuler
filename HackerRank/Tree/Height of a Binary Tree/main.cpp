c6fa2aa4053a492b2f2a09ab149545c62199887e
KristopherWagner <kwagner@novetta.com>
2021-07-30 17:08:24 +0000

    Use correct solution

--- HackerRank/Tree/Height of a Binary Tree/main.cpp
+++ HackerRank/Tree/Height of a Binary Tree/main.cpp
@@ -1,55 +1,59 @@
 // https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree
-class Node:
-    def __init__(self, info): 
-        self.info = info  
-        self.left = None  
-        self.right = None 
-        self.level = None 
+#include <bits/stdc++.h>
 
-    def __str__(self):
-        return str(self.info) 
+using namespace std;
 
-class BinarySearchTree:
-    def __init__(self): 
-        self.root = None
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
 
-    def create(self, val):  
-        if self.root == None:
-            self.root = Node(val)
-        else:
-            current = self.root
-         
-            while True:
-                if val < current.info:
-                    if current.left:
-                        current = current.left
-                    else:
-                        current.left = Node(val)
-                        break
-                elif val > current.info:
-                    if current.right:
-                        current = current.right
-                    else:
-                        current.right = Node(val)
-                        break
-                else:
-                    break
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
 
-"""
-Node is defined as
-self.left (the left child of the node)
-self.right (the right child of the node)
-self.info (the value of the node)
-"""
-def levelOrder(root):
-    queue = [root]
-    printOrder = []
-    while len(queue) != 0:
-        cur = queue.pop(0)
-        printOrder.append(cur.info)
-        if cur.left:
-            queue.append(cur.left)
-        if cur.right:
-            queue.append(cur.right)
-    print(*printOrder)
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
 
+*/
+    int height(Node* root) {
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if (root->left != NULL) {
+            leftHeight = 1 + height(root->left);
+        }
+        if (root->right != NULL) {
+            rightHeight = 1 + height(root->right);
+        }
+        if (leftHeight > rightHeight) {
+            return leftHeight;
+        }
+        return rightHeight;
+    }
+
+};
