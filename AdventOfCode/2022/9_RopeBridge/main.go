73f5b91d7ae5b4c24c406ade3a8d4a49e08a3e6f
KristopherWagner <kmwagner91@gmail.com>
2022-12-13 11:25:37 -0500

    Add solution to day 9 part 2
    https://adventofcode.com/2022/day/9

--- AdventOfCode/2022/9_RopeBridge/main.go
+++ AdventOfCode/2022/9_RopeBridge/main.go
@@ -14,8 +14,12 @@ type Point struct {
 	y int64
 }
 
+func (p *Point) toString() string {
+	return fmt.Sprintf("(%d, %d)", p.x, p.y)
+}
+
 func getInput() (input []string) {
-	input, _ = helpers.ParseInputFile("test_data.txt")
+	input, _ = helpers.ParseInputFile("input.txt")
 	return
 }
 
@@ -111,17 +115,68 @@ func partOne(data []string) (answer int64) {
 	return
 }
 
-func handleMovementPt2(rope []Point, direction string, units int64, visitedLocations map[Point]int64) ([]Point, map[Point]int64) {
-	// TODO
+func shouldMove(start, end Point) bool {
+	dX := end.x - start.x
+	if dX < 0 {
+		dX = -dX
+	}
+	dY := end.y - start.y
+	if dY < 0 {
+		dY = -dY
+	}
+	return dX > 1 || dY > 1
+}
+
+func updateRope(rope []Point) []Point {
+	for i := 1; i < 10 && shouldMove(rope[i], rope[i-1]); i++ {
+		dx, dy := rope[i-1].x-rope[i].x, rope[i-1].y-rope[i].y
+		if dx < 0 {
+			rope[i].x--
+		} else if dx > 0 {
+			rope[i].x++
+		}
+
+		if dy < 0 {
+			rope[i].y--
+		} else if dy > 0 {
+			rope[i].y++
+		}
+	}
+	return rope
+}
+
+func handleMovementPt2(rope []Point, direction string, units int64, visitedLocations map[Point]bool) ([]Point, map[Point]bool) {
+	for d := int64(0); d < units; d++ {
+		switch direction {
+		case "R":
+			rope[0] = Point{x: rope[0].x + 1, y: rope[0].y}
+			break
+		case "U":
+			rope[0] = Point{x: rope[0].x, y: rope[0].y + 1}
+			break
+		case "L":
+			rope[0] = Point{x: rope[0].x - 1, y: rope[0].y}
+			break
+		case "D":
+			rope[0] = Point{x: rope[0].x, y: rope[0].y - 1}
+			break
+		default:
+			fmt.Printf("Unknown direction: " + direction)
+			break
+		}
+
+		rope = updateRope(rope)
+		visitedLocations[rope[9]] = true
+	}
 	return rope, visitedLocations
 }
 
-func partTwo(data []string) (answer int64) {
+func partTwo(data []string) (answer int) {
 	rope := make([]Point, 10)
 	for i := 0; i < 10; i++ {
 		rope[i] = Point{x: 0, y: 0}
 	}
-	visitedLocations := make(map[Point]int64)
+	visitedLocations := make(map[Point]bool)
 
 	for i := 0; i < len(data) && len(data[i]) > 0; i++ {
 		command := strings.Split(data[i], " ")
@@ -137,11 +192,8 @@ func partTwo(data []string) (answer int64) {
 		}
 		rope, visitedLocations = handleMovementPt2(rope, direction, units, visitedLocations)
 	}
-	for _, value := range visitedLocations {
-		if value > 0 {
-			answer++
-		}
-	}
+
+	answer = len(visitedLocations)
 	return
 }
 
