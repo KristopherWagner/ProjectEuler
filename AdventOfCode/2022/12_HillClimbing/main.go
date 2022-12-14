356a94a52791971b36e144c040f0d3cc8c94f6b5
KristopherWagner <kmwagner91@gmail.com>
2022-12-14 13:49:51 -0500

    Add solution to day 12 part 2
    https://adventofcode.com/2022/day/12

--- AdventOfCode/2022/12_HillClimbing/main.go
+++ AdventOfCode/2022/12_HillClimbing/main.go
@@ -17,12 +17,17 @@ var root *Node
 var endNode *Node
 var allNodes []*Node
 
-func canMoveTo(start, end *Node) bool {
-	dH := end.height - start.height
+func canMoveTo(start, end *Node, part int) bool {
+	var dH int
+	if part == 1 {
+		dH = end.height - start.height
+	} else {
+		dH = start.height - end.height
+	}
 	return dH <= 1
 }
 
-func fillChildren(grid [][]*Node) {
+func fillChildren(grid [][]*Node, part int) {
 	for y := 0; y < len(grid); y++ {
 		for x := 0; x < len(grid[y]); x++ {
 			start := grid[y][x]
@@ -30,28 +35,28 @@ func fillChildren(grid [][]*Node) {
 			// left
 			if x > 0 {
 				end := grid[y][x-1]
-				if canMoveTo(start, end) {
+				if canMoveTo(start, end, part) {
 					start.children = append(start.children, end)
 				}
 			}
 			// right
 			if x != (len(grid[y]) - 1) {
 				end := grid[y][x+1]
-				if canMoveTo(start, end) {
+				if canMoveTo(start, end, part) {
 					start.children = append(start.children, end)
 				}
 			}
 			// up
 			if y > 0 {
 				end := grid[y-1][x]
-				if canMoveTo(start, end) {
+				if canMoveTo(start, end, part) {
 					start.children = append(start.children, end)
 				}
 			}
 			// down
 			if y != (len(grid) - 1) {
 				end := grid[y+1][x]
-				if canMoveTo(start, end) {
+				if canMoveTo(start, end, part) {
 					start.children = append(start.children, end)
 				}
 			}
@@ -85,7 +90,7 @@ func fillTree(data []string) (err error) {
 		err = fmt.Errorf("did not find start")
 		return
 	}
-	fillChildren(grid)
+	fillChildren(grid, 1)
 	return
 }
 
@@ -126,28 +131,82 @@ func partOne(data []string) (answer int) {
 	return
 }
 
+func fillTreePt2(data []string) (err error) {
+	grid := make([][]*Node, 0)
+	for y := 0; y < len(data) && len(data[y]) > 0; y++ {
+		grid = append(grid, make([]*Node, len(data[y])))
+		for x := 0; x < len(data[y]); x++ {
+			n := &Node{
+				children: make([]*Node, 0),
+				distance: 2147483647,
+				height:   int(data[y][x]),
+			}
+			grid[y][x] = n
+			if n.height == 83 {
+				n.height = 97
+			} else if n.height == 69 {
+				n.height = 122
+				root = n
+			}
+			allNodes = append(allNodes, n)
+		}
+	}
+
+	if root == nil {
+		err = fmt.Errorf("did not find start")
+		return
+	}
+	fillChildren(grid, 2)
+	return
+}
+
 func partTwo(data []string) (answer int) {
+	err := fillTreePt2(data)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	root.distance = 0
+	fmt.Println("Root is -> " + string(rune(root.height)))
+	findEnd(root)
+	var lowestNode *Node
+	for i := 0; i < len(allNodes); i++ {
+		if allNodes[i].height == 97 {
+			if lowestNode == nil {
+				lowestNode = allNodes[i]
+			} else if allNodes[i].distance < lowestNode.distance {
+				lowestNode = allNodes[i]
+			}
+		}
+	}
+	answer = lowestNode.distance
 	return
 }
 
 func main() {
-	test, err := helpers.ParseInputFile("test_data.txt")
-	if err != nil {
-		fmt.Println("Unable to open test file: " + err.Error())
-		return
-	}
+	/*
+		test, err := helpers.ParseInputFile("test_data.txt")
+		if err != nil {
+			fmt.Println("Unable to open test file: " + err.Error())
+			return
+		}
+	*/
 	input, err := helpers.ParseInputFile("input.txt")
 	if err != nil {
 		fmt.Println("Unable to open input file: " + err.Error())
 		return
 	}
-
-	part1TestAnswer := 31
-	testResult := partOne(test)
-	fmt.Printf("%d -> %t\n", testResult, testResult == part1TestAnswer)
-	fmt.Printf("Part 1: %d\n", partOne(input))
-
-	part2TestAnswer := 29
-	fmt.Println(partTwo(test) == part2TestAnswer)
+	/*
+		part1TestAnswer := 31
+		testResult := partOne(test)
+		fmt.Printf("%d -> %t\n", testResult, testResult == part1TestAnswer)
+		fmt.Printf("Part 1: %d\n", partOne(input))
+	*/
+	/*
+		part2TestAnswer := 29
+		testResult := partTwo(test)
+		fmt.Printf("%d -> %t\n", testResult, testResult == part2TestAnswer)
+	*/
 	fmt.Printf("Part 2: %d\n", partTwo(input))
 }
